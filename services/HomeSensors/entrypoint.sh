93f1e0cac6714833483468240c647ce63c87a5c0
fullonic <somnium@riseup.net>
2019-10-14 17:07:16 +0200

    Celery worker runs in a separate container [worker]

--- services/HomeSensors/entrypoint.sh
+++ services/HomeSensors/entrypoint.sh
@@ -8,8 +8,8 @@ done
 
 echo "PostgreSQL started"
 
-echo "STARTING CELERY"
-celery worker -A celery_worker -l info &
+# echo "STARTING CELERY"
+# celery worker -A celery_worker -l info &
 echo "STARTING APP"
 python manage.py run -h 0.0.0.0
 # gunicorn 0.0.0.0:5000 manage:app
