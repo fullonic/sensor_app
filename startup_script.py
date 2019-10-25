1d862e86cfdb6bc0334d69ee3d9d01b1459dd1d1
fullonic <somnium@riseup.net>
2019-10-25 18:38:59 +0200

    Add startup and build scripts

--- startup_script.py
+++ startup_script.py
@@ -1,4 +1,20 @@
+"""
+Script to run app on raspberry pi start up.
+
+This script will be called using the rc.local file.
+
+Edit the file:
+sudo nano /etc/rc.local
+
+Add startup script to the rc.local. Must be the full path:
+
+# SENSOR APP STARTUP SCRIPT
+python3 /home/pi/Desktop/sensor_app/startup_script.py
+
+"""
+
 import subprocess
 
 
-command = "docker-compose -f <FULLPATH OF DOCKER-COMPOSE FILE> up -d"
+command = "docker-compose -f /home/pi/Desktop/sensor_app/docker-compose.yml up -d"
+subprocess.Popen(command, shell=True)
